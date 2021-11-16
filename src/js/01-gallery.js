efaa72154202d4a6cfde214f8de1223afa1b22cb
Marina Boikova <boymar84@gmail.com>
2021-11-16 16:14:59 +0200

    Добавляет библиотеку SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,27 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryItemsRef = document.querySelector('.gallery');
+
+const galleryItemMarcup = galleryItems.map(item => 
+    `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a> `
+);
+
+galleryItemsRef.insertAdjacentHTML('beforeend',  galleryItemMarcup.join(''));
+
+const onHandleLargePhoto = e => 
+    e.preventDefault();
+   let lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250
+    });
+
+galleryItemsRef.addEventListener('click', onHandleLargePhoto);
+
