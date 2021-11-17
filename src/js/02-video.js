2dcae9ce4ef1a0809492759d575acbe85c8c556a
Marina Boikova <boymar84@gmail.com>
2021-11-17 11:27:13 +0200

    Добавляет Задачу 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from "@vimeo/player";
+import Throttle from "lodash.throttle";
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds)
+    console.log(onPlay)
+};
+
+player.on('timeupdate', Throttle(onPlay, 1000));
+
+
+const playerCurrenTime = localStorage.getItem('videoplayer-current-time');
+
+
+
+if (playerCurrenTime) {
+    player.setCurrentTime(playerCurrenTime);
+}
+else {
+    player.setCurrentTime(0);
+
+}
